d002900017618a422492a059b169fc9dd539cadf
Brijesk <brijeshk_raj@yahoo.com>
2020-03-26 14:56:16 +0530

    added missing file

---
+++ test/__init__.py
@@ -0,0 +1,7 @@
+"""Koninklijke Philips N.V., 2019 - 2020. All rights reserved."""
+
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join
+                                (os.path.dirname(__file__), os.pardir, "functiondefextractor"
+                                 )))
