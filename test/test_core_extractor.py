c342982c02165d58d1739cdf0ef00ea602d1ab88
Reddy <sannihith.reddyp@philips.com>
2020-06-15 14:23:15 +0530

    Refactored test_cmd_log file

--- test/test_core_extractor.py
+++ test/test_core_extractor.py
@@ -101,7 +101,7 @@ class SimpleTest(unittest.TestCase):
                                               "expeccodeextractor_T_T.xlsx")).sort_values('Uniq ID').values.tolist()
         df2_list = pd.read_excel(os.path.join(os.path.dirname(__file__), os.pardir, "test_resource",
                                               "codeextractor_T_T.xlsx")).sort_values('Uniq ID').values.tolist()
-        self.assertEqual(df1_list, df2_list)
+        self.assertEqual(str(df1_list), str(df2_list).replace("\r", ""))
         os.remove(os.path.join(os.path.dirname(__file__), os.pardir, "test_resource", "expeccodeextractor_T_T.xlsx"))
 
     def test_invalid_path(self):
