27d2dd490324586114c61e252c2068f920792b51
Reddy <sannihith.reddyp@philips.com>
2020-07-28 09:39:12 +0530

    corrected whl file installation

--- build_scripts/verify_path.py
+++ build_scripts/verify_path.py
@@ -5,7 +5,7 @@ import os
 import sys
 import unittest
 
-sys.path.append(os.path.dirname(__file__))
+sys.path.append(os.path.join(os.path.dirname(__file__), "..", "test"))
 from test.test_core_extractor import SimpleTest  # pylint: disable=C0413
 
 
