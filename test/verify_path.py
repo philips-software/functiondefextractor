26f26bba354d7b675ee5cb961cadae153e54e0f7
Reddy <sannihith.reddyp@philips.com>
2020-07-28 11:21:00 +0530

    corrected sanity test

--- test/verify_path.py
+++ test/verify_path.py
@@ -3,15 +3,25 @@ file to verify the functional tests."""
 
 import os
 import unittest
-from test_core_extractor import SimpleTest  # pylint: disable=C0413
+
+import pandas as pd
 
 
 class FunctionalTestVerification(unittest.TestCase):
     """
     class consolidates the functional test verification
     """
-    file_path = (os.path.join(os.path.dirname(__file__), os.pardir)).split("test")[0]
-
     def verify_functional_test(self):
         """ This function verifies the result populated from the functional test """
-        SimpleTest.test_process_extract(self)
+        my_dir = os.path.join(os.path.dirname(__file__), os.pardir, "test_resource")
+        for fname in os.listdir(my_dir):
+            if fname.startswith("ExtractedFunc_"):
+                df1_list = pd.read_excel(os.path.join(os.path.dirname(__file__), os.pardir, "test_resource",
+                                                      fname)).sort_values('Uniq ID')
+                df2_list = pd.read_excel(os.path.join(os.path.dirname(__file__), os.pardir, "test_resource",
+                                                      "codeextractor_T_T_A.xlsx")).sort_values('Uniq ID')
+                df1_list["Code"] = df1_list["Code"].str.replace(os.linesep, "")
+                df2_list["Code"] = df2_list["Code"].str.replace(os.linesep, "")
+                df2_list["Code"] = df2_list["Code"].str.replace("\r", "")
+                self.assertEqual(df1_list["Code"].values.tolist().sort(), df2_list["Code"].values.tolist().sort())
+                os.remove(os.path.join(os.path.dirname(__file__), os.pardir, "test_resource", fname))
