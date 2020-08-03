4899241ad941baaf85e44d73a68478fb14c8bf9f
Reddy <sannihith.reddyp@philips.com>
2020-08-03 15:29:55 +0530

    updated coverage upload token

--- build_scripts/dependencies_static_analysis_test_cov.py
+++ build_scripts/dependencies_static_analysis_test_cov.py
@@ -109,18 +109,17 @@ def test_coverage():
     """
     call_subprocess('python3 -m pytest test --cov-config=.coveragerc --cov-report "html" --cov=functiondefextractor')
     call_subprocess("coverage report --fail-under=95")
-    # call_subprocess("codecov")
     print("Stage test & coverage -- COMPLETED & PASSED --")
 
 
-# def mutation_testing():
-#     """
-#     executes the mutation tests and gates for 20 percentage
-#     """
-#     call_subprocess("python3 -m mutmut run > mutmut.log || true")
-#     call_subprocess("mutmut junitxml --suspicious-policy=ignore --untested-policy=ignore > mutmut.xml")
-#     call_subprocess("python3 build_scripts/mutmut_parse.py --m 20")
-#     print("Stage mutation testing -- COMPLETED & PASSED  --")
+def mutation_testing():
+    """
+    executes the mutation tests and gates for 20 percentage
+    """
+    call_subprocess("python3 -m mutmut run > mutmut.log || true")
+    call_subprocess("mutmut junitxml --suspicious-policy=ignore --untested-policy=ignore > mutmut.xml")
+    call_subprocess("python3 build_scripts/mutmut_parse.py --m 20")
+    print("Stage mutation testing -- COMPLETED & PASSED  --")
 
 
 if __name__ == "__main__":
@@ -136,4 +135,4 @@ if __name__ == "__main__":
     check_dead_code()
     check_spelling()
     test_coverage()
-    # mutation_testing()
+    mutation_testing()
