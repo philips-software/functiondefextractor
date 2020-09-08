8eb410994c5561fac152358cd01c9b6a6d634206
Reddy <sannihith.reddyp@philips.com>
2020-09-08 09:15:47 +0530

    Final Mutation

--- functiondefextractor/__main__.py
+++ functiondefextractor/__main__.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
     if ARGS.conditionchecker is None:
         validate_inputs(ARGS.path, "repository")
         ARGS.reportpath = ARGS.path if ARGS.reportpath is None else ARGS.reportpath
-        validate_inputs(ARGS.reportpath, "report folder")  # pragma: no mutate
+        validate_inputs(ARGS.reportpath, "report folder")
         get_report(extractor(ARGS.path, ARGS.annot, ARGS.delta, ARGS.funcstartwith, ARGS.reportpath, ARGS.ignorefiles)
                    , ARGS.reportpath)
     else:
